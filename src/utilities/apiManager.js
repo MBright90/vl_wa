6f97d7a8c8fdf8897337db4e786d02dbe119a5e5
mbright90 <michael.bright90@icloud.com>
2023-12-07 10:31:33 +0000

    Initiate ApiManager file

---
+++ src/utilities/apiManager.js
@@ -0,0 +1,11 @@
+// const weatherApiKey = '43df7ed317e5646ac516d5c73acdd3fc';
+
+const ApiManager = (() => {
+  const getCurrentTemp = () => {
+
+  };
+
+  return getCurrentTemp;
+})();
+
+export default ApiManager;
