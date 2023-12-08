305c39f08a85582a6a1d8dd852bec7b800e3bd39
mbright90 <michael.bright90@icloud.com>
2023-12-08 10:26:39 +0000

    Lay out initial query and response

--- src/utilities/apiManager.js
+++ src/utilities/apiManager.js
@@ -1,11 +1,17 @@
-// const weatherApiKey = '43df7ed317e5646ac516d5c73acdd3fc';
+const weatherKey = '43df7ed317e5646ac516d5c73acdd3fc';
 
 const ApiManager = (() => {
-  const getCurrentTemp = () => {
+  const newportQuery = `https://api.openweathermap.org/geo/1.0/direct?q=Newport&limit=1&appid=${weatherKey}`;
 
+  const getCurrentTemp = async () => {
+    const response = await fetch(newportQuery);
+
+    if (!response.ok) return false;
+
+    return response;
   };
 
-  return getCurrentTemp;
+  return { getCurrentTemp };
 })();
 
 export default ApiManager;
