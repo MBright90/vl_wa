83c0691a1110db19cb4d1308899507b08ff611a9
mbright90 <michael.bright90@icloud.com>
2023-12-09 14:15:25 +0000

    Resolve fetch promise and parse temp data

--- src/utilities/apiManager.js
+++ src/utilities/apiManager.js
@@ -1,14 +1,18 @@
 const weatherKey = '43df7ed317e5646ac516d5c73acdd3fc';
+// Newport location data
+const lat = 51.5842;
+const lon = 2.9977;
 
 const ApiManager = (() => {
-  const newportQuery = `https://api.openweathermap.org/geo/1.0/direct?q=Newport&limit=1&appid=${weatherKey}`;
+  const newportQuery = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherKey}&units=metric`;
 
   const getCurrentTemp = async () => {
     const response = await fetch(newportQuery);
+    const weatherData = await response.json();
 
     if (!response.ok) return false;
 
-    return response;
+    return Math.round(weatherData.list[0].main.temp);
   };
 
   return { getCurrentTemp };
