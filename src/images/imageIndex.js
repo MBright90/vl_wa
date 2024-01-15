59fc3b084e10dc10b9f0626154973cf23903f4ce
mbright90 <michael.bright90@icloud.com>
2024-01-15 14:19:48 +0000

    Remove image background import

--- src/images/imageIndex.js
+++ src/images/imageIndex.js
@@ -3,8 +3,7 @@ import bg1 from './clockBg1.jpeg';
 import bg2 from './clockBg2.jpeg';
 import bg3 from './clockBg3.jpeg';
 import bg4 from './clockBg4.jpeg';
-import bg5 from './clockBg5.jpeg';
 
-const imageArr = [bg0, bg1, bg2, bg3, bg4, bg5];
+const imageArr = [bg0, bg1, bg2, bg3, bg4];
 
 export default imageArr;
