6e000ebcc1f81e4b47adaa8ae1964735e4ea8c10
mbright90 <michael.bright90@icloud.com>
2024-02-17 10:56:55 +0000

    Add new background images

--- src/images/imageIndex.js
+++ src/images/imageIndex.js
@@ -1,9 +1,10 @@
-import bg0 from './clockBg.jpeg';
-import bg1 from './clockBg1.jpeg';
-import bg2 from './clockBg2.jpeg';
-import bg3 from './clockBg3.jpeg';
 import bg4 from './clockBg4.jpeg';
+import bg5 from './clockBg5.jpeg';
+import bg6 from './clockBg6.jpeg';
+import bg7 from './clockBg7.jpeg';
+import bg8 from './clockBg8.jpeg';
+import bg9 from './clockBg9.jpeg';
 
-const imageArr = [bg0, bg1, bg2, bg3, bg4];
+const imageArr = [bg4, bg5, bg6, bg7, bg8, bg9];
 
 export default imageArr;
