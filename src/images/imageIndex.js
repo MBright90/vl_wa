198754969eab25047292aac68b0bac868c569c52
mbright90 <michael.bright90@icloud.com>
2024-02-20 16:05:24 +0000

    Add weather images and export from index

--- src/images/imageIndex.js
+++ src/images/imageIndex.js
@@ -4,7 +4,13 @@ import bg6 from './clockBg6.jpeg';
 import bg7 from './clockBg7.jpeg';
 import bg8 from './clockBg8.jpeg';
 import bg9 from './clockBg9.jpeg';
+import sunImg from './sunshine.png';
+import cloudImg from './cloud.jpg';
 
 const imageArr = [bg4, bg5, bg6, bg7, bg8, bg9];
+export const weatherImgs = {
+  sun: sunImg,
+  cloud: cloudImg,
+};
 
 export default imageArr;
